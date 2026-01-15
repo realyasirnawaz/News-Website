eb9f5cdaca6abe4e82847652ed9334fcf22b1c71
Yasir Nawaz <contacttoyasirkhan@gmail.com>
2026-01-15 07:38:10 +0500

    news website

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ let cardsContaner = document.querySelector('.childCard');
 const value = document.getElementById('navInput');
 let navbtn = document.querySelector('#navbtn');
 
+ 
  function handlesearch(event){
   event.preventDefault();
-  
-  let inputvalue = value.value
-  console.log(inputvalue)
+//  let inputvalue = value.value|| "usa"
   newaApi()
- 
-//     fecth API    //
+ }
 
-
-let api = ""
+newaApi()
+// fetch API
 async function newaApi() {
-    let apifecth = await fetch (`https://newsdata.io/api/1/latest?apikey=${}&q=${inputvalue} | ${"usa"}`)
+  let api = "pub_802ca099cef34f76aaba233fbe221c7e"
+    let inputvalue = value.value|| "usa"
+    let apifecth = await fetch (`https://newsdata.io/api/1/latest?apikey=${api}&q=${inputvalue}`)
     let apiResult = await apifecth.json()
     console.log(apiResult)
     const {results} = apiResult
@@ -28,15 +28,17 @@ async function newaApi() {
     results.forEach(element => {
        
      //         creat cards         //
-
+        let {image_url,title,description,link}= element;
+        
+        console.log(image_url,title,description,link)
         const card = document.createElement('div')
         card.classList.add('new-card');
          
         card.innerHTML = `<div class="card" style="width: 18rem;">
-  <img src=${element.image_url} | ${"https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=612x612&w=0&k=20&c=9pR2-nDBhb7cOvvZU_VdgkMmPJXrBQ4rB1AkTXxRIKM="}class="card-img-top" alt="">
+  <img src=${element.image_url}class="card-img-top" alt="">
   <div class="card-body">
-    <h5 class="card-title">${element.title}</h5>
-    <p class="card-text">${element.description.slice(0,150)}</p>
+    <h5 class="card-title">${element.title.slice(0,70)}</h5>
+    <p class="card-text">${description.slice(0,150) || "No description Found"}</p>
     <div class="cardsBtn">
     <a href=${element.link} class="btn btn-primary">Read More</a>
     <button type="button" class="btn btn-light">${moment(element.pubDate).fromNow()}</button>
@@ -45,10 +47,11 @@ async function newaApi() {
   </div>
 ` 
         cardsContaner.appendChild(card)
+        // console.log("function ended")
     });
    
 }
- }
+ 
 
 
 
